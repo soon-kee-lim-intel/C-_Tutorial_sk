32c2bf2a9fcbf06063a7929be7d01ae4d4307da4
Lim, Soon Kee <soon.kee.lim@intel.com>
2024-11-21 16:26:13 +0800

    First training

--- Demo_FirstSession/Program.cs
+++ Demo_FirstSession/Program.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace Demo_FirstSession
 {
-    internal class Program
+    using Demo_2ndNamespace;
+    using System.Security.Cryptography.X509Certificates;
+
+    class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello Worlds.");
+
+            // Print the press enter to continue line and pause the command prompt.
+            Console.WriteLine("Press enter to continue");
+            Console.ReadLine();
+
+            Demo_2ndNamespace.Program.Main2();
+            Console.ReadLine();
+        }
+         
+        public static void Main2()
+        {
+            Console.WriteLine("Press enter to exit main 2.");
+        }
+    }
+}
+
+namespace Demo_2ndNamespace
+{
+    public class Program
+    {
+        public static void Main2()
+        {
+            Console.WriteLine("Press enter to exit main 2 in 2ndNameSpace.");
         }
     }
 }
